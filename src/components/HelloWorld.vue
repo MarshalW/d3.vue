1856039cb417b9c8bea859f55be79c63dd8988d8
Marshal <marshal.wu@qq.com>
2018-08-28 18:33:37 +0800

    add v3 version helloworld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,107 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="graphArea">
+        <button @click="handleButtonClick">切换</button>
+        <br/>
+        <svg>
+            <line x1="40" x2="560" y1="250" y2="250" stroke="#ddd"/>
+        </svg>
+    </div>
 </template>
 
 <script>
+import * as d3 from 'd3'
+
+let dataSource = [
+    ['中国', [30, 45, 88, 23]],
+    ['美国', [40, 48, 72, 100, 34]],
+    ['日本', [10, 33, 26]],
+]
+let lineLength = 400
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'HelloWorld',
+    data () {
+        return {
+            currentIndex: 0
+        }
+    },
+    mounted () {
+        let svg = d3.select(this.$el).select('svg')
+        svg.append('text').attr('x', 250).attr('y', 50).text(dataSource[this.currentIndex][0])
+
+        //创建圆
+        svg.append('g').selectAll('circle')
+            .data(dataSource[this.currentIndex][1])
+            .enter()
+            .append('circle')
+            .attr('class', 'myCircle')
+            .attr('cx', (d, i) => {
+                let spacing = lineLength / dataSource[this.currentIndex][1].length
+                return 100 + i * spacing
+            })
+            .attr('cy', 250)
+            .attr('r', (d) => {
+                return d
+            })
+    },
+    methods: {
+        handleButtonClick () {
+            this.currentIndex++
+            if (this.currentIndex == dataSource.length) {
+                this.currentIndex = 0
+            }
+
+            let svg = d3.select(this.$el).select('svg')
+            svg.select('text').text(dataSource[this.currentIndex][0])
+
+            let circle = svg.select('g').selectAll('circle').data(dataSource[this.currentIndex][1])
+            circle.exit().remove()
+            let newCircle = circle.enter().append('circle')
+                .attr('class', 'myCircle')
+                .attr('r', 0)
+                .attr('cx', (d, i) => {
+                    let spacing = lineLength / dataSource[this.currentIndex][1].length
+                    return 100 + i * spacing
+                })
+            circle = newCircle.merge(circle)
+
+            circle.transition().duration(500)
+                .attr('cx', (d, i) => {
+                    let spacing = lineLength / dataSource[this.currentIndex][1].length
+                    return 100 + i * spacing
+                })
+                .attr('cy', 250)
+                .attr('r', (d) => {
+                    return d
+                })
+            svg.select('text').text(dataSource[this.currentIndex][0])
+        }
+    }
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+    .graphArea {
+        display: flex;
+        flex-direction: column;
+    }
+
+    button {
+        width: 40px;
+    }
+
+    svg {
+        width: 600px;
+        height: 500px;
+        border: 1px;
+        border-color: gray;
+        border-style: solid;
+    }
+
+    .myCircle {
+        fill: orange;
+        stroke: orange;
+        fill-opacity: 0.5;
+    }
 </style>
